325e1030a1d310e0547cccabab344f9c1d3b9b5f
Jerry <42112870+Jerry-onGithub@users.noreply.github.com>
2022-08-06 17:20:14 +0800

    Update saveToS3.py

--- saveToS3.py
+++ saveToS3.py
@@ -18,24 +18,25 @@ from botocore.exceptions import ClientError
 import os
 
 
-name="1234"+".csv"
-s3 = boto3.resource('s3', aws_access_key_id="AKIAW3S276MPSQLCK2NI", aws_secret_access_key= "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")
-bucket = 'jbot' 
-client = boto3.client("s3", aws_access_key_id="AKIAW3S276MPSQLCK2NI", aws_secret_access_key= "nFfsptRUqTGBoLidyjPTG4VlbY4SejBuIsOH4S9n")
+name="1234"+".csv"  #name of your file
+s3 = boto3.resource('s3', aws_access_key_id="", aws_secret_access_key= "")  #set your aws_access_key_id and aws_secret_access_key
+bucket = ''     #your bucket name
+client = boto3.client("s3", aws_access_key_id="", aws_secret_access_key= "")    #set your aws_access_key_id and aws_secret_access_key
 
+#save file function
 def saveFile():
-    data=[[1, 1, 1]]
-    df = pd.DataFrame(data, columns=['a', 'b', 'c'])
+    data=[[1, 1, 1]]     #set this to your data
+    df = pd.DataFrame(data, columns=['a', 'b', 'c'])    #can change this your column values
     csv_buffer = StringIO()
     df.to_csv(csv_buffer)
     
     #s3.Object(bucket, name).put(Body=csv_buffer.getvalue())
-    s3.upload_file('/tmp/' + name, bucket, 'orders_csv/{}'.format(name))
+    s3.upload_file('/tmp/' + name, bucket, 'orders_csv/{}'.format(name))    #change 'orders_csv' to the name of your path location
 
 
-
+#check if file already exists 
 def check():
-    file='604873.csv'
+    file='604873.csv'   #name of file to be checked
     try:
         s3.Object(bucket, file).load()
         
@@ -60,13 +61,15 @@ def check():
             raise
         return False
 
+#if you want to delete file, you can use this function
 def delete(key): 
     s3.Object(bucket, key).delete()
     
+#upload an image
 def uploadImage():
-    filename='user_photo.jpg'
+    filename='user_photo.jpg'   #name of image to be uploaded
     try:
-        response = client.upload_file('C:/Users/Jerry/Desktop/user_photo.jpg', bucket, 'orders_images/user_photo.jpg')
+        response = client.upload_file('C:/Users/.../.../user_photo.jpg', bucket, 'orders_images/user_photo.jpg')    #change the local path and S3 path according to your own
     
     except ClientError as e:
         logging.error(e)
